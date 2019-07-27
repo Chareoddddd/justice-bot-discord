8ddde77dec847aca0f320f47f8f7f49a07ee0141
Chareoddddd <samy.2659@gmail.com>
2019-07-27 09:18:02 +0200

    Update App.java

--- src/main/java/Justice/JusticeBot/App.java
+++ src/main/java/Justice/JusticeBot/App.java
@@ -219,24 +219,6 @@ public class App extends ListenerAdapter
     		String imageUrl = "";
     		String uri = "https://rule34.xxx/?page=dapi&s=post&q=index&limit=100&tags=";
     		Random rand = new Random();
-		
-		String[] tmp = e.getMessage().getContentRaw().split(Character.toString('"'), 0);
-		int size = 0;
-
-		for (int i = 0; i < tmp.length; i++) {
-			if (!tmp[i].equals(" ")) {
-				size++;
-			}
-		}
-		
-		int r = 0;
-		String[] orders = new String[size];
-		for (int i = 0; i < tmp.length; i++) {
-			if (!tmp[i].equals(" ")) {
-				orders[r] = tmp[i];
-				r++;
-			}
-		}
     		
     		if (orders.length >= 2) {
     			tag = tag + orders[1];
