a33956e93068782614f8d5c85aa83bd12b157aea
Chareoddddd <samy.2659@gmail.com>
2019-07-28 14:58:01 +0200

    Update App.java

--- src/main/java/Justice/JusticeBot/App.java
+++ src/main/java/Justice/JusticeBot/App.java
@@ -117,7 +117,7 @@ public class App extends ListenerAdapter
 		build.setColor(0xaa51e2);
 		
 		build.setTitle("**Liste des Commandes**");
-		build.addField(prefix + "\"prefixe\"", "Modifie le préfixe", false);
+		build.addField(prefix + "prefixe \"nouveau\"", "Modifie le préfixe", false);
 		/*build.addField(prefix + "punir", "Enlève la citoyenneté et met en Sous-Race (Administrateur requis)", false);
 		build.addField(prefix + "pardon", "Enlève Sous-Race et met la citoyenneté (Administrateur requis)", false);*/
 		build.addField(prefix + "poll \"Question\" \"Réponse 1\" \"Réponse 2\" ...", "Effectue un poll", false);
